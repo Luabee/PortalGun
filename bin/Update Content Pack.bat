7c6a6734ee1f21899529ad94271a541609b5f599
Bob Blackmon <bob.blackmon@ymail.com>
2014-11-03 21:13:30 -0500

    Made standalone
    no portal or portal 2 required (except for the fuckin gay arms.)

---
+++ bin/Update Content Pack.bat
@@ -0,0 +1,4 @@
+
+gmad.exe create -folder "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\DMG TTT & DarkRP Content Pack" -out "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons"
+"Update Content Pack.bat" > log.txt 2>&1
+pause
