631e684f2c267f5876fa0ea90a6d1ca6e735dfcd
Bob Blackmon <bob.blackmon@ymail.com>
2014-11-03 21:14:37 -0500

    Removed useless items

--- bin/Update Content Pack.bat
+++
@@ -1,4 +0,0 @@
-
-gmad.exe create -folder "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\DMG TTT & DarkRP Content Pack" -out "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons"
-"Update Content Pack.bat" > log.txt 2>&1
-pause
--- bin/Upload Content Pack.bat
+++
@@ -1,4 +0,0 @@
-
-gmpublish.exe update -addon "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons.gma" -id "133176410" -changes "Added map icons"
-"Upload Content Pack.bat" > log.txt 2>&1
-pause
